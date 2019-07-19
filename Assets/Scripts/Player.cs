1b5c1cef769a03e5bf53106e607487c5e3563b75
frogred <452823574@qq.com>
2019-07-19 11:58:59 +0800

    first commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    public float moveSpeed = 3;
+
+    private SpriteRenderer sr;
+
+    private void Awake()
+    {
+        sr = GetComponent<SpriteRenderer>();//上右下左
+    }
+
+    public Sprite[] tankSprite;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+    }
+    private void FixedUpdate()//帧数固定
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        transform.Translate(Vector3.right * h * moveSpeed * Time.fixedDeltaTime, Space.World);
+        if (h < 0)
+        {
+            sr.sprite = tankSprite[3];
+        }
+        else if (h > 0)
+        {
+            sr.sprite = tankSprite[1];
+        }
+        float v = Input.GetAxisRaw("Vertical");
+        transform.Translate(Vector3.up * v * moveSpeed * Time.fixedDeltaTime, Space.World);
+        if (v < 0)
+        {
+            sr.sprite = tankSprite[2];
+        }
+        else if (v > 0)
+        {
+            sr.sprite = tankSprite[0];
+        }
+    }
+}
